ecc4a3e338f4eb379cb1afacbd489f6e1ea74825
Torstein Solheim Ølberg <mr.torstein@gmail.com>
2019-12-09 17:32:53 +0100

    Nå da?

--- Scripts/lib.h
+++ Scripts/lib.h
@@ -4,8 +4,27 @@
 
 class lib
 {
-public:
-    lib();
+    public:
+        void Set_up_lists(int, int, int, int, int);
+
+        double S_f(double, double, double, double, double);
+        double I_f(double, double, double, double, double);
+
+        void RK4(double, double, int , int);
+
+        int Pop;
+        int Pop_S;
+        int Pop_I;
+        int Pop_R;
+        int N;
+
+        double ** S [4];
+        double ** I [4];
+        double ** R [4];
+
+        double * a = new double [4];
+        double * b = new double [4];
+        double * c = new double [4];
 };
 
 #endif // LIB_H
